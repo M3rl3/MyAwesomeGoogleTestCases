9652eff47e587ea818af7b3cf3b1aa17b685aeb6
M3rl3 <rameezkhatri17.rk@gmail.com>
2022-09-18 20:12:37 -0400

    Added Black Box test cases

---
+++ BlackBoxTestCases/BlackBoxTest.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "BlackBoxTest.h"
+
+
+int Invert(int num) {
+	return (num *= -1);
+}
--- MyAwesomeGoogleTestCases/Main.cpp
+++ MyAwesomeGoogleTestCases/Main.cpp
@@ -2,7 +2,9 @@
 #include <string>
 
 #include <gtest/gtest.h>
+#include <BlackBoxTestCases/BlackBoxTest.h>
 
+//Returns number of digits in the passed number
 int Count(int num) {
 	int index = 0;
 	while (num != 0) {
@@ -14,11 +16,15 @@ int Count(int num) {
 }
 
 TEST(count, numbers) {
-	EXPECT_EQ(1, Count(5));
+	EXPECT_EQ(1, Count(6));
+	EXPECT_EQ(3, Count(420));
+	EXPECT_EQ(4, Count(69));
 }
 
-TEST(count1, numbers1) {
-	EXPECT_EQ(2, Count(420));
+TEST(invert, numbers) {
+	EXPECT_EQ(-5, Invert(5));
+	EXPECT_EQ(-2, Invert(25));
+	EXPECT_EQ(-125, Invert(125));
 }
 
 int main(int argc, char** argv) {
